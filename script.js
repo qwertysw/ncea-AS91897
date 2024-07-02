e44fa820b54204a9884fef524330f4ef5c1d7aed
qwertysw <88912140+qwertysw@users.noreply.github.com>
2024-07-02 17:41:36 +1200

    Hotjar bug fixing

--- script.js
+++ script.js
@@ -134,4 +134,15 @@ document.addEventListener('DOMContentLoaded', function() {
             footer.style.bottom = '-100px';
         }
     });
+
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5031963,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+
 });
